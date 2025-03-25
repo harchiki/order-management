2fa35dfeeeb313bf91da893b1f210afe56949c11
samet.yildiz <sametyildiz03@gmail.com>
2025-03-25 23:54:30 +0300

    Added config folder

---
+++ config-server/src/test/java/com/order/management/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.management.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
