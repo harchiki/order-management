7c51d2245a016a35009c2e2889e287968acb0ecf
samet.yildiz <sametyildiz03@gmail.com>
2025-04-03 23:10:06 +0300

    Renamed stock-service to product-service

--- product-service/src/main/java/com/order/management/stockservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/order/management/stockservice/ProductServiceApplication.java
@@ -1,14 +1,13 @@
 package com.order.management.stockservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockServiceApplication {
+public class ProductServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockServiceApplication.class, args);
+		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
 }
