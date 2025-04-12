535a7bba95f9c17238d5e347987d369ef4fdd948
samet.yildiz <sametyildiz03@gmail.com>
2025-04-12 22:58:06 +0300

    Added price queue and its configurations

---
+++ accounting-service/src/main/java/com/order/management/accountingservice/config/RabbitmqSchemaConfig.java
@@ -0,0 +1,28 @@
+package com.order.management.accountingservice.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Declarables;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class RabbitmqSchemaConfig {
+	@Bean
+	Declarables createPriceSchema(@Value("${order.accounting.price.queue}") String priceQueueName,
+								  @Value("${order.accounting.price.exchange}") String priceQueueExchangeName) {
+		DirectExchange directExchange = new DirectExchange(priceQueueExchangeName, true,
+				false, null);
+
+		Queue priceQueue = new Queue(priceQueueName);
+		Binding binding = BindingBuilder.bind(priceQueue).to(directExchange).withQueueName();
+		return new Declarables(directExchange, priceQueue, binding);
+	}
+}
