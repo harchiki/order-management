23e6e7de86954a2e54532f50d68e802e23d0b284
samet.yildiz <sametyildiz03@gmail.com>
2025-04-12 22:51:33 +0300

    Integrated discount calls into accounting service

--- accounting-service/src/main/java/com/order/management/accountingservice/service/impl/AccountingServiceImpl.java
+++ accounting-service/src/main/java/com/order/management/accountingservice/service/impl/AccountingServiceImpl.java
@@ -2,6 +2,9 @@ package com.order.management.accountingservice.service.impl;
 
 import com.order.management.accountingservice.dto.OrderPrice;
 import com.order.management.accountingservice.dto.OrderRecordDto;
+import com.order.management.accountingservice.service.DiscountService;
+import com.order.management.accountingservice.service.external.DiscountClient;
+import com.order.management.accountingservice.service.external.dto.DiscountResponseDto;
 import com.order.management.accountingservice.service.external.dto.ProductPriceDto;
 import com.order.management.accountingservice.dto.ProductRequestDto;
 import com.order.management.accountingservice.dto.ProductTotalPriceDto;
@@ -20,6 +23,7 @@ import java.util.List;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class AccountingServiceImpl implements AccountingService {
     private final ProductService productService;
+    private final DiscountService discountService;
     private final ProductPriceMapper productPriceMapper;
     private final OrderPriceMapper orderPriceMapper;
 
@@ -38,8 +42,10 @@ public class AccountingServiceImpl implements AccountingService {
         // map OrderPrice
         OrderPrice orderPrice = orderPriceMapper.orderRecordDtoToOrderPriceDto(orderRecordDto);
         orderPrice.setCart(productTotalPriceDtos);
-        // todo
-        orderPrice.setDiscount(100);
+
+        // get discount and set amount discount
+        DiscountResponseDto discount = discountService.getDiscountDetail(orderRecordDto.getDiscountCode());
+        orderPrice.setDiscount(discount.getDiscountAmount());
 
         // calculate order total prices
         orderPrice.setTotalRawPrice(orderPrice.getCart().stream().mapToDouble(ProductTotalPriceDto::getPrice).sum());
@@ -49,7 +55,6 @@ public class AccountingServiceImpl implements AccountingService {
         orderPrice.setStatus(OrderStatus.PRICED);
 
         // todo send it payment queue
-
     }
 
     private void setProductPriceAndTotal(ProductTotalPriceDto totalPriceDto, List<ProductPriceDto> productPriceDtos) {
